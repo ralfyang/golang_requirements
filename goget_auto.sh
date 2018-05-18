4fd88d926998e7717dd054438e4a0141e6f9be84
Ralf Yang <goody80762@gmail.com>
2018-05-18 18:31:49 +0900

    script for the golang import check and get

---
+++ goget_auto.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+WORKDIR="[PLEASE MAKE SURE THE WORKDIR]"
+$(cat $WORKDIR/main.go | xargs 2> /dev/null | sed -e 's/^package.*.import (//g' -e 's/ ).*//g' -e 's/ /\n/g' |egrep "^github" | awk '{print "go get",$1}')
