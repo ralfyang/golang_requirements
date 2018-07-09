3be97b3bb47517a66044a98052d570d95da6b5a4
Ralf Yang <goody80762@gmail.com>
2018-07-09 10:27:57 +0900

    Update goget_auto.sh

--- goget_auto.sh
+++ goget_auto.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-WORKDIR="[PLEASE MAKE SURE THE WORKDIR]"
+#WORKDIR="[PLEASE MAKE SURE THE WORKDIR]"
+WORKDIR="./"
 $(cat $WORKDIR/main.go | xargs 2> /dev/null | sed -e 's/^package.*.import (//g' -e 's/ ).*//g' -e 's/ /\n/g' |egrep "^github" | awk '{print "go get",$1}')
