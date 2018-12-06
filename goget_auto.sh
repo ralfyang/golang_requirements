98fc4504df634d0f15fcf024d8893d85c8613cc6
Ralf Yang <goody80762@gmail.com>
2018-12-06 15:26:44 +0900

    Update goget_auto.sh

--- goget_auto.sh
+++ goget_auto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #WORKDIR="[PLEASE MAKE SURE THE WORKDIR]"
 WORKDIR="./"
-$(cat $WORKDIR/main.go | xargs 2> /dev/null | sed -e 's/^package.*.import (//g' -e 's/ ).*//g' -e 's/ /\n/g' |egrep "^github" | awk '{print "go get",$1}')
+eval $(cat $WORKDIR/main.go | xargs 2> /dev/null | sed -e 's/^package.*.import (//g' -e 's/ ).*//g' -e $'s/ /\\\n/g' |egrep "^github" | awk '{print "go get",$1,";"}' | xargs | sed -e 's/;$//g')
