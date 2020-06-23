67ee80c4233022896837a84d1468c71e532c0e3e
Ralf Yang <goody80762@gmail.com>
2020-06-23 15:27:22 +0900

    Update goget_auto.sh

--- goget_auto.sh
+++ goget_auto.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## Code from https://github.com/ralfyang/golang_requirements. Powered by Github.
+
 #WORKDIR="[PLEASE MAKE SURE THE WORKDIR]"
 WORKDIR="./"
 eval $(cat $WORKDIR/main.go | xargs 2> /dev/null | sed -e 's/^package.*.import (//g' -e 's/ ).*//g' -e $'s/ /\\\n/g' |egrep "^github" | awk '{print "go get",$1,";"}' | xargs | sed -e 's/;$//g')
